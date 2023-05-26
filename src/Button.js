edc56783e80344e6f310f657fac2c3b975cdf7be
Daniel Vadil <dvadz@yahoo.com>
2023-05-26 16:55:21 -0400

    Used classname package to assist in putting together a bunch of tailwind css classes to style Button

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 const Button = ({
   children,
   primary,
@@ -8,9 +10,31 @@ const Button = ({
   outline,
   rounded,
 }) => {
+  let classes = classNames(
+    // default, always applied
+    "border-2 px-3 py-1.5 m-1",
+    {
+      //  variant
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-600 bg-gray-600 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      // rounded
+      "rounded-full": rounded,
+      // outline
+      "bg-white": outline,
+      "text-blue-600": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-600": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-600": outline && danger,
+    }
+  );
+
   return (
     <div>
-      <button>{children}</button>
+      <button className={classes}>{children}</button>
     </div>
   );
 };
