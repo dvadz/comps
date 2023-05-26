00b1df82df468c3ab7378905108f137173f8c64e
Daniel Vadil <dvadz@yahoo.com>
2023-05-26 16:56:22 -0400

    Added a bunc hof buttons to show all Button variations

--- src/App.js
+++ src/App.js
@@ -2,10 +2,71 @@ import Button from "./Button";
 
 const App = () => {
   return (
-    <div>
-      <Button primary outline rounded>
-        Click Me!
-      </Button>
+    <div style={{ columns: 4 }}>
+      <div>
+        <Button>Plain</Button>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+        <Button success>Success</Button>
+        <Button warning>Warning</Button>
+        <Button danger>Danger </Button>
+      </div>
+      <div>
+        <Button rounded>Plain</Button>
+        <Button primary rounded>
+          Primary
+        </Button>
+        <Button secondary rounded>
+          Secondary
+        </Button>
+        <Button success rounded>
+          Success
+        </Button>
+        <Button warning rounded>
+          Warning
+        </Button>
+        <Button danger rounded>
+          Danger
+        </Button>
+      </div>
+      <div>
+        <Button outline>Plain</Button>
+        <Button primary outline>
+          Primary
+        </Button>
+        <Button secondary outline>
+          Secondary
+        </Button>
+        <Button success outline>
+          Success
+        </Button>
+        <Button warning outline>
+          Warning
+        </Button>
+        <Button danger outline>
+          Danger
+        </Button>
+      </div>
+      <div>
+        <Button outline rounded>
+          Plain
+        </Button>
+        <Button primary rounded outline>
+          Primary
+        </Button>
+        <Button secondary rounded outline>
+          Secondary
+        </Button>
+        <Button success rounded outline>
+          Success
+        </Button>
+        <Button warning rounded outline>
+          Warning
+        </Button>
+        <Button danger rounded outline>
+          Danger
+        </Button>
+      </div>
     </div>
   );
 };
