2850e7234e8a3d1e732a4a2b1018d78a3af4fb64
Daniel Vadil <dvadz@yahoo.com>
2023-05-27 16:42:42 -0400

    Fixed placement of button icons by adding some classes to have them align with the text

--- src/Button.js
+++ src/Button.js
@@ -12,7 +12,7 @@ const Button = ({
 }) => {
   let classes = classNames(
     // default, always applied
-    "border-2 px-3 py-1.5 m-1",
+    "border-2 px-3 py-1.5 m-1 flex items-center",
     {
       //  variant
       "border-blue-500 bg-blue-500 text-white": primary,
