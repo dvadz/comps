5248422429f098c066ef819fe8ef87d462fd1783
Daniel Vadil <dvadz@yahoo.com>
2023-06-03 01:10:46 -0400

    Created an array of items for the Accordion to show

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import Accordion from "./components/Accordion";
 
 const App = () => {
-  return <Accordion />;
+  const items = [
+    {
+      id: "sdsge",
+      show: false,
+      label: "What is react?",
+      content: "You know... it is what it is",
+    },
+    {
+      id: "wiotf",
+      show: false,
+      label: "Statement #2?",
+      content: "Look it up on google. You know what to do!",
+    },
+    {
+      id: "lnvjf",
+      show: false,
+      label: "Roses are red, violets are blue",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, dolore.",
+    },
+  ];
+  return <Accordion items={items} />;
 };
 
 export default App;
