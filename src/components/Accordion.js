82aeff9a7ccea2c9957a80f90cc77f603a334895
Daniel Vadil <dvadz@yahoo.com>
2023-06-03 01:15:05 -0400

    Created a piece of state that will track which item in the Accordion is expanded

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Accordion = ({ items }) => {
+  const [expandedItem, setExpandedItem] = useState(0);
+
   const renderedItems = items.map((item) => {
     return (
       <div key={item.id}>
