fe494ee69bdc9f30c8062a4ee2a514603a796446
Daniel Vadil <dvadz@yahoo.com>
2023-06-03 16:59:49 -0400

    Added chevron icons to indicate whether item is expanded or collapsed

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { BiChevronSquare, BiChevronDownSquare } from "react-icons/bi";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 const Accordion = ({ items }) => {
   const [expandedItem, setExpandedItem] = useState(0);
 
   const renderedItems = items.map((item, index) => {
     const content = index === expandedItem ? <div>{item.content}</div> : false;
+    const icon = index === expandedItem ? <GoChevronDown /> : <GoChevronLeft />;
     return (
       <div key={item.id}>
         <div
@@ -12,7 +15,8 @@ const Accordion = ({ items }) => {
             setExpandedItem(index);
           }}
         >
-          {item.label}
+          <span>{item.label}</span>
+          <span>{icon}</span>
         </div>
         {content}
       </div>
