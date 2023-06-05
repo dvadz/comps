cf05945ecf9b690940d122a9f3525bab2c281cba
Daniel Vadil <dvadz@yahoo.com>
2023-06-05 00:54:03 -0400

    Moved everything in App into a new page called AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import Accordion from "./components/Accordion";
-
 const App = () => {
-  const items = [
-    {
-      id: "sdsge",
-      show: false,
-      label: "What is react?",
-      content: "You know... it is what it is",
-    },
-    {
-      id: "wiotf",
-      show: false,
-      label: "Statement #2?",
-      content: "Look it up on google. You know what to do!",
-    },
-    {
-      id: "lnvjf",
-      show: false,
-      label: "Roses are red, violets are blue",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, dolore.",
-    },
-  ];
-  return <Accordion items={items} />;
+  return <div></div>;
 };
 
 export default App;
