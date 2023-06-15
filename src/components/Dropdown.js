7a41eec7826d1ddf7346a8ec1ab19a1702c31719
Daniel Vadil <dvadz@yahoo.com>
2023-06-15 01:51:04 -0400

    Styled the dropdown all at once, big mess

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,6 +11,7 @@ const Dropdown = ({ options, value, onChange }) => {
   const renderedOptions = options.map((option) => {
     return (
       <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
         key={option.value}
         onClick={(event) => {
           handleOptionClick(option);
@@ -26,9 +27,18 @@ const Dropdown = ({ options, value, onChange }) => {
   };
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || "Select..."}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        <div>{value?.label || "Select..."}</div>
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 };
