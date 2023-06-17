67f471349fb8b1be58f97a03c492a7ae353f3ae1
Daniel Vadil <dvadz@yahoo.com>
2023-06-17 00:42:57 -0400

    Combined default class of Panel component and any additional classes passed to Panel

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,16 @@
 import classNames from "classnames";
 
 const Panel = ({ children, className, ...rest }) => {
-  return <div {...rest}> {children}</div>;
+  const finalClassNames = classNames(
+    "border rounded p-3 shadow bg-white w-full",
+    className
+  );
+
+  return (
+    <div className={finalClassNames} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default Panel;
