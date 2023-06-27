064733aa5cac0cebca0e99b81a9d87f851abf8ab
Daniel Vadil <dvadz@yahoo.com>
2023-06-27 12:10:26 -0400

    Created state that holds the pathname

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentName] = useState(window.location.pathname);
+
   return (
     <NavigationContext.Provider value={{}}>
       {children}
