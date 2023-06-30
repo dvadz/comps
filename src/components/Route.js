1535e078bda23be3ac97910ec5015326c38337b0
Daniel Vadil <dvadz@yahoo.com>
2023-06-30 15:01:22 -0400

    Read currentPath from NavigationContext

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,3 +1,8 @@
-const Route = ({ path, children }) => {};
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+const Route = ({ path, children }) => {
+  const { currentPath } = useContext(NavigationContext);
+};
 
 export default Route;
