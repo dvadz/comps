d9c463246ae0debf5a9604065d03fa5bd4b29b6c
Daniel Vadil <dvadz@yahoo.com>
2023-06-30 01:40:09 -0400

    Return an anchor tag that has a click handler that calls on the navigate function in NavigationContext

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,3 +1,15 @@
-const Link = ({ to, children }) => {};
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+const Link = ({ to, children }) => {
+  const { navigate } = useContext(NavigationContext);
+
+  const handler = (event) => {
+    event.preventDefault();
+    navigate(to);
+  };
+
+  return <a onClick={handler}>{children}</a>;
+};
 
 export default Link;
