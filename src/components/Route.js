663aa6be39697fac2a533e0ed6a60358fda78af9
Daniel Vadil <dvadz@yahoo.com>
2023-06-30 15:02:45 -0400

    Show children if path and currenthPath are the same

--- src/components/Route.js
+++ src/components/Route.js
@@ -3,6 +3,11 @@ import NavigationContext from "../context/navigation";
 
 const Route = ({ path, children }) => {
   const { currentPath } = useContext(NavigationContext);
+
+  if (path === currentPath) {
+    return children;
+  }
+  return null;
 };
 
 export default Route;
