9cb83601e4250340a20a901de07679914eb25f70
Daniel Vadil <dvadz@yahoo.com>
2023-07-01 00:56:19 -0400

    Used useNavigation hook in Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,14 +1,11 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
 import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
 const Link = ({ to, children }) => {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
 
   const handler = (event) => {
-    console.log(event);
     if (event.metaKey || event.ctrlKey || event.shiftKey) {
-      console.log("Command");
       return;
     }
     event.preventDefault();
