6dc2657f9d1e7cec244308722ede4aaf58c705e6
Daniel Vadil <dvadz@yahoo.com>
2023-07-01 02:02:09 -0400

    Added an array of links and rendered each into a Link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,21 @@
-const Sidebar = () => {};
+import Link from "./Link";
+
+const Sidebar = () => {
+  const links = [
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buttons" },
+  ];
+
+  const renderedLinks = links.map((link) => {
+    return (
+      <Link key={link.label} to={link.path}>
+        {link.label}
+      </Link>
+    );
+  });
+
+  return <div> {renderedLinks}</div>;
+};
 
 export default Sidebar;
