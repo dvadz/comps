7cd495187ece63ea71a1186a855c9cc4f9f7fbef
Daniel Vadil <dvadz@yahoo.com>
2023-07-02 22:20:58 -0400

    Placed Sidebar in App and added corresponding Route components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import Route from "./components/Route";
+import Sidebar from "./components/Sidebar";
+import DropdownPage from "./pages/DropdownPage";
+import AccordionPage from "./pages/AccordionPage";
+import ButtonsPage from "./pages/ButtonPage";
+
 const App = () => {
-  return <div>App </div>;
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        <Route path={"/"}>
+          <DropdownPage />
+        </Route>
+        <Route path={"/accordion"}>
+          <AccordionPage />
+        </Route>
+        <Route path={"/buttons"}>
+          <ButtonsPage />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
