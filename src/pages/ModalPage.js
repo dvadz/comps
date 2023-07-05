f02c4209b8d8ccf7361ba71e09ec653be14bfda1
Daniel Vadil <dvadz@yahoo.com>
2023-07-05 17:21:19 -0400

    Added a state that keeps track whether to show Modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div>
       <Button primary rounded>
