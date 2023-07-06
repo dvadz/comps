50dc1faf7c058e9451019ce9dda54d98ad33b5db
Daniel Vadil <dvadz@yahoo.com>
2023-07-06 12:55:08 -0400

    Added TailWindCSS styles to create a basic modal, it works but just by pure luck and will be modified later

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,10 @@
 const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">This is a Modal</div>
+    </div>
+  );
 };
 
 export default Modal;
