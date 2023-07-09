378e0d04d15b27397cf1f25b846371ccb276f3fe
Daniel Vadil <dvadz@yahoo.com>
2023-07-09 18:31:47 -0400

    Added actionBar to Modal component, accepts buttons

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom";
 
-const Modal = ({ onClose, children }) => {
+const Modal = ({ onClose, children, actionBar }) => {
   return ReactDOM.createPortal(
     <div>
       <div
         className="absolute inset-0 bg-gray-300 opacity-80"
         onClick={onClose}
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">{children}</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        <div>{actionBar}</div>
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
