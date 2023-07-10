b548170d31a451a053280ac7b36cf8ac539127fb
Daniel Vadil <dvadz@yahoo.com>
2023-07-10 15:39:49 -0400

    Replaced absolute with fixed so that Modal is visible if placed at the bottom of a long page

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,10 +13,10 @@ const Modal = ({ onClose, children, actionBar }) => {
   return ReactDOM.createPortal(
     <div>
       <div
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
         onClick={onClose}
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
