942aa1c9e093fa87e9920a55a9dfde64edc9c569
Daniel Vadil <dvadz@yahoo.com>
2023-07-11 16:08:01 -0400

    Created an array in TablePage, passed it then received in the Table

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,7 +1,14 @@
 import Table from "../components/Table";
 
+const data = [
+  { name: "orange", color: "bg-orange-500", score: 4 },
+  { name: "banana", color: "bg-yellow-500", score: 1 },
+  { name: "lime", color: "bg-green-500", score: 5 },
+  { name: "apple", color: "bg-red-500", score: 3 },
+];
+
 const TablePage = () => {
-  return <Table />;
+  return <Table data={data} />;
 };
 
 export default TablePage;
