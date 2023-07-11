8a7b421231aad317c7fba2517be3039ef9b1ea25
Daniel Vadil <dvadz@yahoo.com>
2023-07-11 16:09:04 -0400

    Manually built the fruit table in Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,28 @@
 const Table = ({ data }) => {
-  return <div>{data.length}</div>;
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Fruit</th>
+            <th>Color</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
