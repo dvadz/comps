71b8a60bd445215f924ed74d098acc1a40591d28
Daniel Vadil <dvadz@yahoo.com>
2023-07-18 09:22:00 -0400

    Update column label if config has sortValue

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -3,7 +3,18 @@ import Table from "./Table";
 const SortableTable = (props) => {
   const { config } = props;
 
-  return <Table {...props} />;
+  const updatedConfig = config.map((column) => {
+    console.log(column);
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => <th> {column.label} IS SORTABLE</th>,
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
 };
 
 export default SortableTable;
