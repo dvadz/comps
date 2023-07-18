c8db402456a37e3c2971e652777ed52aafc9a921
Daniel Vadil <dvadz@yahoo.com>
2023-07-18 15:57:56 -0400

    Added a click event handler to the sortable headers

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -3,14 +3,21 @@ import Table from "./Table";
 const SortableTable = (props) => {
   const { config } = props;
 
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
   const updatedConfig = config.map((column) => {
-    console.log(column);
     if (!column.sortValue) {
       return column;
     }
     return {
       ...column,
-      header: () => <th> {column.label} IS SORTABLE</th>,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} IS SORTABLE
+        </th>
+      ),
     };
   });
 
