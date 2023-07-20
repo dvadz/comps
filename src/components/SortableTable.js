6a6b021e94bffb20e056c216447026130c7712a2
Daniel Vadil <dvadz@yahoo.com>
2023-07-20 01:18:54 -0400

    Show pointer and background color when hovering over sortable columns

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -84,7 +84,10 @@ const SortableTable = (props) => {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => handleClick(column.label)}
+        >
           <div className="flex items-center">
             {getIcons(column.label)}
             {column.label}
